21d6b724c86910989b45e97095201cc6a47846b2
NahidHasan131 <nahid7532140@gmail.com>
2026-04-09 12:51:17 +0600

    applayout modify

--- src/routes/HomeLayout/AppLayout.tsx
+++ src/routes/HomeLayout/AppLayout.tsx
@@ -5,7 +5,7 @@ import HomeFooter from "./HomeFooter";
 const AppLayout = () => {
     return (
         <div >
-            <marquee className="bg-[#f0f5e8] p-1 text-black/70%">Big sale, Mega Offer! Get 20% Flat Discount for All Product</marquee>
+            {/* <marquee className="bg-[#f0f5e8] p-1 text-black/70%">Big sale, Mega Offer! Get 20% Flat Discount for All Product</marquee> */}
             <HomeNav/>
             <Outlet/>
             <HomeFooter/>
